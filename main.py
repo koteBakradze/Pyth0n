0978fc1628aba44b9ecd2e003b100b2328ff1d91
Konstantine Bakradze <133692172+koteBakradze@users.noreply.github.com>
2023-11-04 17:35:14 +0000

    კითხვარი

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello world")
+name = input("რა გქვია? ")
+lastName = input("რა გვარის ხარ? ")
+year = input("რამდენი წლის ხარ? ")
+answer = ""
+#String ანუ str() არის სიტყვები 
+#INtegers ანუ int() არის ციფრები
+
+
+# git add . ვამატებ ფაილებს
+# git commit -m "აქ დავწერ კომენტარს ჩემ ცვლილებებზე"
+# git push ჩემ ცვლილებებს ავტვირთავ
+
+print(f"გამარჯობა {name} {lastName} \t")
+if int(year) < 18:
+    answer = "მაპატიე ზედმეტად ახალგაზრდა ხარ"
+else:
+    answer = "რამხელა ყოფილხარ ბიჭოოოოოო"
+print(answer)
